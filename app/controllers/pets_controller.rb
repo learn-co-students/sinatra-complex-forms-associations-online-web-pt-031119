e515cf0a59adc15fd64ef950f3d2f288be1c81dd
github username <github email address>
2019-06-23 22:03:01 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,7 +2,7 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
   get '/pets/new' do
@@ -10,7 +10,7 @@ class PetsController < ApplicationController
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
     @pet = Pet.create(params[:pet])
     if !params["owner"]["name"].empty?
       @owner = Owner.create(name: params["owner"]["name"])
@@ -20,26 +20,18 @@ class PetsController < ApplicationController
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  get '/pets/:id/edit' do 
+  get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
     @owners = Owner.all
     erb :'/pets/edit'
   end
 
-  patch '/pets/:id' do 
-    ####### bug fix
-    # if !params[:pet].keys.include?("owner_id")
-    # params[:pet]["owner_id"] = []
-    # end
-    ####### required so that it's possible to remove ALL previous pets from owner
-    binding.pry
-  
-    @pet = Pet.find(params[:id])
+  patch '/pets/:id' do  @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
       @owner = Owner.create(name: params["owner"]["name"])
