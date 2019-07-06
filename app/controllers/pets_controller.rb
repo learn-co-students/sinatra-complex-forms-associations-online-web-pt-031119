25b11e0b155b05cd8f39ee8da980fdc0e4de7d3f
MKanko <mkanko66@gmail.com>
2019-07-05 18:07:25 -0700

    completed sinatra-complex-forms-associations-lab

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -42,12 +42,17 @@ class PetsController < ApplicationController
   end 
 
   patch '/pets/:id' do
-    binding.pry
-    if !params[:pet].keys.include?("owner_ids")
-      params[:pet]["owner_ids"] = []
-       
-      end 
+    #binding.pry
+    # if !params[:pet].keys.include?("owner_ids")
+    #   params[:pet]["owner_id"] = []      
+    #end 
 
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"]) 
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save  
     redirect to "pets/#{@pet.id}"
   end
 end
