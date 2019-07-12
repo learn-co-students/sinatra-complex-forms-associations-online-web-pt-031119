1c172f44d07f6d1c9c21d0d28d9a59ee109deea3
Dennis Chung <chungdennis6@gmail.com>
2019-07-12 15:40:52 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -7,10 +7,16 @@ class OwnersController < ApplicationController
 
   get '/owners/new' do 
     erb :'/owners/new'
+    @pets = Pet.all
   end
 
   post '/owners' do 
-    
+    @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    @owner.save
+    redirect to "owners/#{@owner.id}" 
   end
 
   get '/owners/:id/edit' do 
@@ -23,7 +29,12 @@ class OwnersController < ApplicationController
     erb :'/owners/show'
   end
 
-  patch '/owners/:id' do 
-   
+  post '/owners/:id' do
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect to "owners/#{@owner.id}"
   end
 end
